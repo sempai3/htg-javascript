74039af2f2efdc84ceaf3e843dff4eefabe0b4f4
jenn-lang <jennifervallejos08@gmail.com>
2020-01-10 16:31:42 -0300

    termine ejercicos menos lo tecnico

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++
@@ -1,1 +0,0 @@
-console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
